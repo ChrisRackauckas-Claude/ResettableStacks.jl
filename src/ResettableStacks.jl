659e2ad2d0fd3be716fb03507f4c4fec29ab5fec
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-08-20 08:32:59 -0400

    Add representative PrecompileTools workload
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- src/ResettableStacks.jl
+++ src/ResettableStacks.jl
@@ -10,5 +10,19 @@ module ResettableStacks
 
     include("core.jl")
 
+    using PrecompileTools: @compile_workload, @setup_workload
+
+    @setup_workload begin
+        @compile_workload begin
+            stack = ResettableStack(Float64)
+            push!(stack, 1.0)
+            copyat_or_push!(stack, 2.0)
+            collect(stack)
+            pop!(stack)
+            reset!(stack, true)
+            isempty(stack)
+        end
+    end
+
     export ResettableStack, copyat_or_push!, reset!
 end # module
